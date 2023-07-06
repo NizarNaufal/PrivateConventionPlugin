e9b45badccd966c736c8612d2e139371410d9fe9
telkomdev-nizar <nizarnfl21@gmail.com>
2023-07-06 12:21:48 +0700

    Update gitignore & Update Compose Convention

--- src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -1,5 +1,10 @@
-package id.technzr.privateconventionplugin
-
+import com.android.build.api.dsl.ApplicationExtension
+import id.technzr.configuration.ConventionVersion
+import id.technzr.configuration.configureAndroidCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         with(project) {
